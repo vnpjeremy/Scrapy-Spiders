b1061a673aa503f5747915eac81a3bfad3a08f0f
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-30 18:51:35 -0500

    get the XPath working for the job count parser

--- CareerBuilder Spiders/crawlCareerBuilder/crawlCareerBuilder/spiders/careerBuilder.py
+++ CareerBuilder Spiders/crawlCareerBuilder/crawlCareerBuilder/spiders/careerBuilder.py
@@ -1,4 +1,8 @@
 import scrapy
+from scrapy.selector import Selector
+from crawlCareerBuilder.items import CareerBuilderItem
+from scrapy.http import Request
+import re
 
 
 class Region:
@@ -15,10 +19,30 @@ class CareerBuilder(scrapy.Spider):
     name = "careerBuilder"
     allowed_domains = ["www.careerbuilder.com"]
 
-    start_urls = []
+    software_base_url1 = "http://www.careerbuilder.com/jobs-software-in-"
+    software_base_url2 = "?page_number="
 
+    regions = []
+    regions.append(Region("dallas-fort-worth,tx"))
+
+    for state in regions:
+        state.url_software = software_base_url1 + state.name + software_base_url2
+
+    start_urls = []
+    for state in regions:
+        start_urls.append(state.url_software + "0")
+
+    # Note: for the careerbuilder site, page 0 and 1 will be identical
     def parse(self, response):
-        pass
+        depth = parse_page_max_depth(self, response)
+        depth = depth
 
     def parse_results_page(self, response):
         pass
+
+
+def parse_page_max_depth(self, response):
+    sel = Selector(response)
+    count = sel.xpath('//div[@class="count"]/text()').extract()[0]
+    arg = 1 + 2
+    return 0
